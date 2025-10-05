6ef1f44d05556f6c3509cde35f3b0c54b9998354
Lukas Korotaj <lu0026ko-s@student.lu.se>
2025-10-05 10:43:49 +0200

    Added a script in hopes of merging the branches.

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -3,3 +3,4 @@ source $OMARCHY_INSTALL/helpers/presentation.sh
 source $OMARCHY_INSTALL/helpers/errors.sh
 source $OMARCHY_INSTALL/helpers/logging.sh
 source $OMARCHY_INSTALL/helpers/identification.sh
+source $OMARCHY_INSTALL/helpers/disk-setup.sh
